1639f601034c6e0730c20271949df21ae1d84cea
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-03 21:27:40 -0700

    solution found

--- RecentCounter.java
+++ RecentCounter.java
@@ -1,8 +1,22 @@
+import java.util.*;
+
 public class RecentCounter {
+    Deque<Integer> requests;
+
     public RecentCounter() {
+        requests = new ArrayDeque<>();
     }
 
     public int ping(int t) {
-        return 0;
+        int count = 1;
+        Iterator iterator = requests.descendingIterator();
+        while (iterator.hasNext()) {
+            int val = (int) iterator.next();
+            if (val >= t - 3000 && val <= t)
+                count++;
+        }
+
+        requests.offer(t);
+        return count;
     }
 }
